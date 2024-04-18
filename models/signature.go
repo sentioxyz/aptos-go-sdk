8bf4e31f4cea3477393e0dca3850a380aaedd2e8
lizixing <lzx.lizixing@gmail.com>
2024-04-18 10:42:20 +0800

    signature.type can be single_sender in transaction #1015222711 in testnet

--- models/signature.go
+++ models/signature.go
@@ -1,5 +1,7 @@
 package models
 
+import "encoding/json"
+
 type JSONSignature struct {
 	Type string `json:"type"`
 	ED25519Signature
@@ -12,11 +14,31 @@ type ED25519Signature struct {
 	Signature string `json:"signature"`
 }
 
+type SignatureContent struct {
+	Index     uint64
+	Signature string
+}
+
+func (s *SignatureContent) UnmarshalJSON(raw []byte) error {
+	if len(raw) >= 2 && raw[0] == '"' && raw[len(raw)-1] == '"' {
+		return json.Unmarshal(raw, &s.Signature)
+	}
+	var t struct {
+		Index     uint64 `json:"index"`
+		Signature string `json:"signature"`
+	}
+	if err := json.Unmarshal(raw, &t); err != nil {
+		return err
+	}
+	s.Index, s.Signature = t.Index, t.Signature
+	return nil
+}
+
 type MultiED25519Signature struct {
-	PublicKeys []string `json:"public_keys"`
-	Signatures []string `json:"signatures"`
-	Threshold  uint8    `json:"threshold"`
-	Bitmap     string   `json:"bitmap"`
+	PublicKeys []string           `json:"public_keys"`
+	Signatures []SignatureContent `json:"signatures"`
+	Threshold  uint8              `json:"threshold"`
+	Bitmap     string             `json:"bitmap"`
 }
 
 type MultiAgentSignature struct {
